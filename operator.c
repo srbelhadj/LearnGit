c8916b1217babcc404967dfd2cd861dbdcb444e2
srbelhadj <42896298+srbelhadj@users.noreply.github.com>
2023-08-27 19:27:27 -0700

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  int x,y;
+ 
+  x = 40;
+  y = 30;
+  
+  cout<<"plus" << x+y  << endl;
+}
---
+++ switch.c
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 
+  int x;
+  cout<<"moh \n";
+  
+  cin>>x;
+  
+  switch (x){
+    
+    case 1:
+    cout<<"one";
+    break;
+    
+    case 2:
+    cout<<"two";
+    break;
+     case 3:
+    cout<<"three";
+    break;
+    default :
+    cout<<"bye";
+  }
+}
